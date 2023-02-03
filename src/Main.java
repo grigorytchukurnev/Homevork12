b0fb7874941e44d174bd064776ece5efbb427ebf
Григорий <chukurnev@mail.ru>
2023-02-03 20:25:31 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача1");
+        int age = 13;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " ,то ты достиг совершеннолетия.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " ,то ты не достиг совершеннолетия.");
+        }
     }
 }
